ffb4a384c4a3135a7864f44653abf787ea488d4a
Starving._.Minsung <kms73033222@gmail.com>
2022-06-16 12:11:03 +0900

    ✏️ repeat 함수 사용

--- divisor.js
+++ divisor.js
@@ -3,13 +3,8 @@ process.stdin.on("data", (data) => {
   const n = data.split(" ");
   const a = Number(n[0]),
     b = Number(n[1]);
-
-  let star = "";
+  const row = "*".repeat(a);
   for (let i = 0; i < b; i++) {
-    for (let j = 0; j < a; j++) {
-      star = star + "*";
-    }
-    star = star + "\n";
+    console.log(row);
   }
-  console.log(star);
 });
