f7088a66c3fbbb6491cb0f38b04a4f060758904a
Starving._.Minsung <kms73033222@gmail.com>
2022-06-25 17:13:31 +0900

    프로그래머스 서울에서 김서방 찾기

---
+++ Algorithm/findKim.js
@@ -0,0 +1,9 @@
+//seoul = ["Jane, "Kin"];
+function solution(seoul) {
+  for (i = 0; i < seoul.length; i++) {
+    if (seoul[i] == "Kim") {
+      return `김서방은 ${i}에 있다`;
+    }
+  }
+}
+//return = "김서방은 1에 있다"
