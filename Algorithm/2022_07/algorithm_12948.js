3cfbd3b7deb50a920b0e011c800eb50f0de3d912
Starving._.Minsung <kms73033222@gmail.com>
2022-07-16 20:14:42 +0900

    프로그래머스 핸드폰 번호 가리기

---
+++ Algorithm/2022_07/algorithm_12948.js
@@ -0,0 +1,7 @@
+function solution(phone_number) {
+  phone_number = [...phone_number];
+  for (let i = 0; i < phone_number.length - 4; i++) {
+    phone_number[i] = "*";
+  }
+  return phone_number.join("");
+}
